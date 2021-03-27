1b057284f9fb47eea5935d280f23479893196aae
TreeKR <johanbag290@gmail.com>
2021-03-27 12:47:28 +0900

    Release Candidate 2

--- src/main/kotlin/xyz/namutree0345/deadbydaylight/KillListener.kt
+++ src/main/kotlin/xyz/namutree0345/deadbydaylight/KillListener.kt
@@ -1,11 +1,11 @@
 package xyz.namutree0345.deadbydaylight
 
-import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.potion.PotionEffectType
 
 class KillListener : Listener {
 
@@ -17,6 +17,7 @@ class KillListener : Listener {
                 event.deathMessage = "${ChatColor.RED}살인마가 ${event.entity.name}를 죽였습니다. 남은 인간 수: $remainHumans"
                 event.entity.gameMode = GameMode.SPECTATOR
                 if(remainHumans == 0) {
+                    killer?.removePotionEffect(PotionEffectType.GLOWING)
                     broadcastTitle("${ChatColor.RED}살인마 승리..", "", 60, 180, 180)
                     gameStarted = false
                 }
--- src/main/kotlin/xyz/namutree0345/deadbydaylight/StartCommand.kt
+++ src/main/kotlin/xyz/namutree0345/deadbydaylight/StartCommand.kt
@@ -1,5 +1,7 @@
 package xyz.namutree0345.deadbydaylight
 
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
@@ -9,6 +11,8 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
 import java.util.*
 
 var gameStarted = false
@@ -36,10 +40,20 @@ class StartCommand : CommandExecutor {
             killer = plrs[Random().nextInt(plrs.size)]
             killer?.inventory?.addItem(ItemStack(Material.DIAMOND_AXE))
             Bukkit.broadcastMessage("${ChatColor.RED}살인마: ${killer?.name}")
+            killer?.addPotionEffect(PotionEffect(PotionEffectType.GLOWING, 999999, 255))
             gameStarted = true
             broadcastTitle("${ChatColor.RED}시작!", "", 60, 180, 180)
+            var t = 60 * 5
+            val i = Bukkit.getScheduler().scheduleSyncRepeatingTask(JavaPlugin.getPlugin(DeadByDaylight::class.java), Runnable {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent("$t"))
+                    t -= 1
+                }
+            }, 0, 20)
             Bukkit.getScheduler().scheduleSyncDelayedTask(JavaPlugin.getPlugin(DeadByDaylight::class.java), Runnable {
+                Bukkit.getScheduler().cancelTask(i)
                 if(remainHumans != 0) {
+                    killer?.removePotionEffect(PotionEffectType.GLOWING)
                     broadcastTitle("${ChatColor.GREEN}생존자 승리", "", 60, 180, 180)
                     gameStarted = false
                 }
